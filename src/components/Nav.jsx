514f95ef9c3ad72f5f071ae2d12a643baa1c3430
unknown <lucianopetrate@gmail.com>
2022-08-12 09:04:25 -0300

    responsive for xl screens

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,8 +6,8 @@ import SearchBar from "./SearchBar.jsx";
 
 function Nav({ onSearch }) {
   return (
-    <nav className="inline-grid grid-cols-4 content-center bg-gray-300">
-      <div className="col-start-1 align-items-center">
+    <nav className="inline-grid grid-cols-4 xl:grid-cols-6 content-center bg-gray-300 rounded-md">
+      <div className="col-start-1 align-items-center ml-2">
         <Link to="/">
           <span className="inline-grid grid-cols-2 content-center pt-2">
             <img
@@ -22,7 +22,7 @@ function Nav({ onSearch }) {
           </span>
         </Link>
       </div>
-      <div className="col-end-5 col-span-2">
+      <div className="col-end-5 col-span-2  xl:col-end-7 xl:col-span-3">
         <SearchBar onSearch={onSearch} />
       </div>
     </nav>
